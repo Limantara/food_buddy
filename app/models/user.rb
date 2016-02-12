7d22b7cd7f57d7eae746014bf13dcf08f3cdbea8
Ziyu Huang <h.ziyu@yahoo.com>
2016-02-12 15:39:37 -0800

    add validation for sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+	validates :username, :presence => true
+	validates :username, uniqueness: true
+	validates :password, :presence => true
+	validates :email, :presence => true
+
+	validates :password, length: { minimum: 8 }
+	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
