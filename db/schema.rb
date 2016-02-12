8f27906f005b9ae19f54ed85ff1755d3d96325b6
Limantara <edwin@limantara.me>
2016-02-12 14:47:24 -0800

    Add linking table events_users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212221437) do
+ActiveRecord::Schema.define(version: 20160212224645) do
 
   create_table "events", force: :cascade do |t|
     t.integer  "user_id"
