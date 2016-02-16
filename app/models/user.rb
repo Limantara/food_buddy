33fa8adf3a9731b322397d5c1bbd4eab55908c97
Ziyu Huang <h.ziyu@yahoo.com>
2016-02-16 09:21:32 -0800

    add restriction to the sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@ class User < ActiveRecord::Base
 
 	validates :username, :presence => true
 	validates :username, uniqueness: true
-	validates :password, :presence => true
+	# validates :password, :presence => true
 	validates :email, :presence => true
 
-	validates :password, length: { minimum: 8 }
+	validates :password, :format => {:with => /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/, message: "must be at least 6 characters and include one number and one letter."}
+
 	validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 	has_and_belongs_to_many :events
 
