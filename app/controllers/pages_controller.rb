e8f741bc7bf1f51207752f0eafebcbd3cb7e2ca7
Eric Pham <ericpham380@gmail.com>
2016-02-26 18:02:47 -0800

    Add Yelp API to application

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
-    
+    response = Yelp.client.search('San Jose', { term: 'thai restaurants'})
+    @businesses = response.businesses
   end
 end
