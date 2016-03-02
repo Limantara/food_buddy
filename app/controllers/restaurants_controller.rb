4c7a9eda009d541842d6322d3320df6f6f8829f1
Limantara <edwin.limantara@sjsu.edu>
2016-03-01 17:12:37 -0800

    Add filter when saving yelp data

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -83,12 +83,16 @@ class RestaurantsController < ApplicationController
 
     def save_restaurants
       @businesses.each do |business| 
-        address = "#{business.location.address[0]} <br> " +
-                  "#{business.location.city}, #{business.location.state_code} #{business.location.postal_code }"
+        restaurant = Restaurant.find_by :yelp_id => business.id
 
-        Restaurant.create(:name => business.name, :address => address, :yelp_id => business.id,
-                          :image_url => business.image_url, :review_count => business.review_count,
-                          :rating_img_url => business.rating_img_url)
+        if restaurant.nil?
+          address = "#{business.location.address[0]} <br> " +
+                    "#{business.location.city}, #{business.location.state_code} #{business.location.postal_code }"
+
+          Restaurant.create(:name => business.name, :address => address, :yelp_id => business.id,
+                            :image_url => business.image_url, :review_count => business.review_count,
+                            :rating_img_url => business.rating_img_url)
+        end
       end
     end
 end
