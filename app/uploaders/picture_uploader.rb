b5a6354b8d23259c066a7f8fca0040da40e71353
Eric Pham <ericpham380@gmail.com>
2016-05-05 01:12:21 -0700

    Added profile picture to event card

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -18,12 +18,11 @@ class PictureUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
+   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
+      ActionController::Base.helpers.asset_path([version_name, "https://www.sparklabs.com/forum/styles/comboot/theme/images/default_avatar.jpg"].compact.join('_'))
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+   end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
